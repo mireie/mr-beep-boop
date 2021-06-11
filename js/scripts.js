5a27de0af050e84e8666771e06efd490e0a7d6c1
mireie <michael.reiersgaard@gmail.com>
2021-06-11 16:32:41 -0700

    add custom checkbox functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,23 @@
 function mrBeepBoop() {
   let matchArray = [];
   if (arguments[1] === 1) {
-    matchArray = [0,arguments[2],arguments[3],arguments[4]];
+    matchArray = [0, arguments[2], arguments[3], arguments[4]];
   } else if (arguments[1] === 0) {
-    matchArray = [0,"Beep!","Boop!","Won't you be my neighbor?"];
+    matchArray = [0, "Beep!", "Boop!", "Won't you be my neighbor?"];
   }
   let outputArray = [];
   for (let i = 0; i <= arguments[0]; i++) {
     if (numContains(i) > 0) {
       outputArray.push(matchArray[numContains(i)]);
-    } else 
+    } else
       outputArray.push(i);
   }
   return outputArray;
 }
 
 function numContains(index) {
-  let indexArray=[];
-  let indexString=index.toString();
+  let indexArray = [];
+  let indexString = index.toString();
   let check3 = false;
   let check2 = false;
   let check1 = false;
@@ -31,8 +31,7 @@ function numContains(index) {
       check2 = true;
     } else if (indexArray[i] === "1") {
       check1 = true;
-    } else {
-    }
+    } else { }
   }
   if (check3 === true) {
     return 3;
@@ -45,28 +44,30 @@ function numContains(index) {
   }
 }
 
-//Interface logic
-$(document).ready(function() {
+//Interface Logic
+
+$(document).ready(function () {
   let outputArray = [];
-  $('form#form').submit(function(event) {
+  $('#advanced').click(function () {
+    $('.advanced').slideToggle();
+  });
+  $('form#form').submit(function (event) {
     event.preventDefault();
     const userNumber = parseInt($('#userNumber').val());
     const text1 = $('#text1').val();
     const text2 = $('#text2').val();
     const text3 = $('#text3').val();
-    if (parseInt($('#userNumber').val()) > 1000000) {
-      $('p#output').text("That's a little much, don't you think? Try again with a smaller number or Mr. Beep Boop might explode your browser.");
-    } else if (parseInt($('#userNumber').val()) < 0) {
-      $('p#output').text("I bet you think you're pretty clever, huh? Try again with a positive integer. And not too beeping big, ya hear me, boop?"); 
-    }
-      else {
-        if ($('#advanced').is(":not(:checked)")) {
-          outputArray = mrBeepBoop(userNumber, 0);
-        } else if ($('#advanced').is(":checked"))  {
-          outputArray = mrBeepBoop(userNumber, 1, text1, text2, text3 )
-        }
+    if (userNumber > 1000000) {
+      outputArray = ["That's a little much, don't you think? Try again with a smaller number or Mr. Beep Boop might explode your browser."];
+    } else if (userNumber < 0) {
+      outputArray = ["I bet you think you're pretty clever, huh? Try again with a positive integer. And not too beeping big, ya hear me, boop?"];
+    } else {
+      if ($('#advanced').is(":not(:checked)")) {
+        outputArray = mrBeepBoop(userNumber, 0);
+      } else if ($('#advanced').is(":checked")) {
+        outputArray = mrBeepBoop(userNumber, 1, text1, text2, text3)
       }
+    }
     $('p#output').text(outputArray.join(", "));
-    $('#result').show();
   });
 })
