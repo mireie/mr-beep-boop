5ac4f9572950ed185a53ef5b3fd849d35f7dbb41
mireie <michael.reiersgaard@gmail.com>
2021-06-11 11:11:20 -0700

    add logic to check for 1, 2, 3.

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,27 @@ function mrBeepBoop(input) {
 function numContains(index) {
   let indexArray=[];
   let indexString=index.toString();
-  let output = -1;
+  let check1 = false;
+  let check2 = false;
+  let check3 = false;
   indexArray = indexString.split("");
   for (let i = 0; i < indexArray.length; i++) {
-    if (indexArray[i] === "1") {
-      output = 1;
+    if (indexArray[i] === "3") {
+      check3 = true;
       break;
-    };
+    } else if (indexArray[i] === "2") {
+      check2 = true;
+    } else if (indexArray[i] === "1") {
+      check1 = true;
+    }
   }
-  return output;
+  if (check3 === true) {
+    return 3;
+  } else if (check2 === true) {
+    return 2;
+  } else if (check1 === true) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
