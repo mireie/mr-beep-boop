6ecef8bf674848bedece2e30f8f8955c228ad6bf
mireie <michael.reiersgaard@gmail.com>
2021-06-11 09:46:33 -0700

    add test for array matching

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 function mrBeepBoop(input) {
+  const matchArray = [0,"Beep!","Boop!","Won't you be my neighbor?"];
   let outputArray = [];
   if (input === 0) {
-    outputArray = [0];
-    return outputArray;
+    outputArray = matchArray[0];
+  } else if (input === 1) {
+    outputArray = matchArray[1];
+  } else if (input === 2) {
+    outputArray = matchArray[2];
+  } else if (input === 3) {
+    outputArray = matchArray[3];
   }
+  return outputArray;
 }
