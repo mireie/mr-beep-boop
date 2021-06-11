1ad9969184f67e1f6ee8a8bf05cfc9c4be126503
mireie <michael.reiersgaard@gmail.com>
2021-06-11 11:26:21 -0700

    refactor numContains

--- js/scripts.js
+++ js/scripts.js
@@ -12,27 +12,16 @@ function mrBeepBoop(input) {
 function numContains(index) {
   let indexArray=[];
   let indexString=index.toString();
-  let check1 = false;
-  let check2 = false;
-  let check3 = false;
   indexArray = indexString.split("");
   for (let i = 0; i < indexArray.length; i++) {
     if (indexArray[i] === "3") {
-      check3 = true;
-      break;
+      return 3;
     } else if (indexArray[i] === "2") {
-      check2 = true;
+      return 2;
     } else if (indexArray[i] === "1") {
-      check1 = true;
+      return 1;
+    } else {
+      return 0;
     }
-  }
-  if (check3 === true) {
-    return 3;
-  } else if (check2 === true) {
-    return 2;
-  } else if (check1 === true) {
-    return 1;
-  } else {
-    return 0;
   }
 }
