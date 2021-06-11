0f6618b24c7757bf1019518eef84570860384bb6
mireie <michael.reiersgaard@gmail.com>
2021-06-11 10:01:33 -0700

    add count up with array

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 function mrBeepBoop(input) {
   const matchArray = [0,"Beep!","Boop!","Won't you be my neighbor?"];
   let outputArray = [];
-  if (input === 0) {
-    outputArray = matchArray[0];
-  } else if (input === 1) {
-    outputArray = matchArray[1];
-  } else if (input === 2) {
-    outputArray = matchArray[2];
-  } else if (input === 3) {
-    outputArray = matchArray[3];
+  for (let i = 0; i <= input; i++) {
+    if (i <=3) {
+      outputArray.push(matchArray[i]);
+    } else 
+      outputArray.push(i);
   }
   return outputArray;
 }
