aee5bbee93e68a0e83924f7b14efc874e538c8e4
mireie <michael.reiersgaard@gmail.com>
2021-06-11 16:10:34 -0700

    add logic for custom text entry

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 //Business Logic
-function mrBeepBoop(input) {
-  const matchArray = [0,"Beep!","Boop!","Won't you be my neighbor?"];
+
+function mrBeepBoop() {
+  let matchArray = [];
+  if (arguments[1] === 1) {
+    matchArray = [0,arguments[2],arguments[3],arguments[4]];
+  } else if (arguments[1] === 0) {
+    matchArray = [0,"Beep!","Boop!","Won't you be my neighbor?"];
+  }
   let outputArray = [];
-  for (let i = 0; i <= input; i++) {
+  for (let i = 0; i <= arguments[0]; i++) {
     if (numContains(i) > 0) {
       outputArray.push(matchArray[numContains(i)]);
     } else 
@@ -44,15 +50,23 @@ $(document).ready(function() {
   let outputArray = [];
   $('form#form').submit(function(event) {
     event.preventDefault();
+    const userNumber = parseInt($('#userNumber').val());
+    const text1 = $('#text1').val();
+    const text2 = $('#text2').val();
+    const text3 = $('#text3').val();
     if (parseInt($('#userNumber').val()) > 1000000) {
       $('p#output').text("That's a little much, don't you think? Try again with a smaller number or Mr. Beep Boop might explode your browser.");
     } else if (parseInt($('#userNumber').val()) < 0) {
       $('p#output').text("I bet you think you're pretty clever, huh? Try again with a positive integer. And not too beeping big, ya hear me, boop?"); 
     }
       else {
-      outputArray = mrBeepBoop(parseInt($('#userNumber').val()));
-      $('p#output').text(outputArray.join(", "));
-      $('#result').show();
-    }
+        if ($('#advanced').is(":not(:checked)")) {
+          outputArray = mrBeepBoop(userNumber, 0);
+        } else if ($('#advanced').is(":checked"))  {
+          outputArray = mrBeepBoop(userNumber, 1, text1, text2, text3 )
+        }
+      }
+    $('p#output').text(outputArray.join(", "));
+    $('#result').show();
   });
 })
