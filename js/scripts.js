40bf0f3bbd3aca20cba15938c43fc4365acb2045
mireie <michael.reiersgaard@gmail.com>
2021-06-11 10:52:42 -0700

    add numContains function

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,16 @@ function mrBeepBoop(input) {
   }
   return outputArray;
 }
+function numContains(index) {
+  let indexArray=[];
+  let indexString=index.toString();
+  let output = -1;
+  indexArray = indexString.split("");
+  for (let i = 0; i < indexArray.length; i++) {
+    if (indexArray[i] === "1") {
+      output = 1;
+      break;
+    };
+  }
+  return output;
+}
