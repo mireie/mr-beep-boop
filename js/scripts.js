04a9b4b6a2ef482c248d3bc7217b7b728673f818
mireie <michael.reiersgaard@gmail.com>
2021-06-11 11:56:42 -0700

    connect functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,14 @@ function mrBeepBoop(input) {
   const matchArray = [0,"Beep!","Boop!","Won't you be my neighbor?"];
   let outputArray = [];
   for (let i = 0; i <= input; i++) {
-    if (i <=3) {
-      outputArray.push(matchArray[i]);
+    if (numContains(i) > 0) {
+      outputArray.push(matchArray[numContains(i)]);
     } else 
       outputArray.push(i);
   }
   return outputArray;
 }
+
 function numContains(index) {
   let indexArray=[];
   let indexString=index.toString();
